598171a6c80d99301d0eb2601dcf8dd78a8628bc
Gs1lver <alebasi2005@gmail.com>
2023-02-15 14:44:53 -0300

    switch case e ternária

---
+++ switch.php
@@ -0,0 +1,33 @@
+<?php
+
+$op = 3;
+switch($op){
+    case 1:
+        cadastrar();
+        echo "Cadastro";
+        break;
+    case 2:
+        //emitirRelatórioVenda();
+        echo "Relatorios";
+        break;
+    default:
+        echo "Opcao invalida";
+        break;
+}
+
+echo "<br><br> oi após o break";
+
+$x = 2;
+
+/*if($x%2 == 0){
+    $resultado= "par";
+} else {
+    $resultado = "impar";
+}*/
+
+$resultado = ($x%2)==0 ? "par" : "impar"; //ternária
+
+echo $resultado;
+
+($op == 1) == cadastrar() : consultar(); // pode usar com funções
+?>
