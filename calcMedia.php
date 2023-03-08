6d2c946517e338ea2e32ca294ad663d9030949e0
Gs1lver <alebasi2005@gmail.com>
2023-03-08 08:46:36 -0300

    post get request verificação de formulario em php

--- calcMedia.php
+++ calcMedia.php
@@ -1,3 +1,4 @@
+
 <?php
 function calcMedia($n1, $n2){
     $media = ($n1 + $n2)/2;
@@ -5,17 +6,45 @@ function calcMedia($n1, $n2){
 }
 
 //calcMedia.php?nota1=8.0&nota2=7.0
-$n1 = $_POST["nota1"]; //$_GET $_REQUEST
-$n2 = $_POST["nota2"];
+$n1 = $_GET["nota1"]; //$_GET $_REQUEST$ $_POST - usar get permite que o usuario altere os valores na url!!
+$n2 = $_GET["nota2"];
 
-$media = calcMedia($n1, $n2);
+if((trim($n1) == "") || ( trim($n2) == "") ){
+    echo "<span id='warning'>Informe as duas notas!</span>";
+} else {
+    $media = calcMedia($n1, $n2);
 
-echo "Média = " . $media . "<br>";
+    echo "Média = " . $media . "<br>";
 
-if($media >= 6.0){
-    echo "<span id = 'aprovado'>APROVADO</span>";
-} else{
-    echo "<span id = 'repovado'>REROVADO</span>";
+    if($media >= 6.0){
+        echo "<span id = 'aprovado'>APROVADO</span>";
+    } else{
+        echo "<span id = 'reprovado'>REPROVADO</span>";
+    }
 }
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        #reprovado {
+            color: red;
+        }
+
+        #aprovado {
+            color: green;
+        }
+
+        #warning {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
